c2fe0e14b49e421c4d1387c22bb379ce50c6c785
liangyh <liangyh@spinsoft.com>
2020-07-21 11:22:08 +0800

    tools

--- miniprogram/utils/tools.js
+++ miniprogram/utils/tools.js
@@ -5,16 +5,16 @@ export const randomStr = () => {
   return Math.random().toString(36).substr(2,15)
 }
 /**
- * 
- * @param {String}} url 当前字符串是否在yingyinbi.com域名下
+ * 当前字符串是否在yingyinbi.com域名下
+ * @param {String}} url 
  */
 export const matchQiniuUrl = url => {
   let reg = new RegExp('yingyinbi.com')
   return reg.test(url)
 }
 /**
- * 
- * @param {String}} url 七牛云图片基本处理
+ * 七牛云图片基本处理
+ * @param {String}} url 
  */
 export const imageView = (url) => {
   return url + '?imageView2/2/w/200/h/270'
