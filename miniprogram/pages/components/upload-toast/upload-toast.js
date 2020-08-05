33dec6b1910a6e65832753dbf76cd34ea3f8ab71
liangyh <liangyh@spinsoft.com>
2020-08-05 18:06:22 +0800

    上传提示组件

---
+++ miniprogram/pages/components/upload-toast/upload-toast.js
@@ -0,0 +1,34 @@
+// pages/components/upload-toast/upload-toast.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    progress: {
+      type: Number,
+      value: 90
+    },
+    total: {
+      type: Number,
+      value: 9
+    },
+    now: {
+      type: Number,
+      value: 1
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
